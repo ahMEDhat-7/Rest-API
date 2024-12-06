0555c5b6e1ac35bd279a66ad352552252a2a502e
ahmed medhat <am510836@gmal.com>
2024-12-06 03:25:35 +0200

    Error handeling on getting course by id => invalid obj id

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
-const Course = require('../models/courses.model');
-const {validationResult} = require('express-validator');
-
+const fs = require("fs");
+const Course = require("../models/courses.model");
+const { validationResult } = require("express-validator");
 
 const getAllCourses = async (req, res) => {
   const courses = await Course.find();
@@ -9,12 +8,15 @@ const getAllCourses = async (req, res) => {
 };
 
 const getSingleCourse = async (req, res) => {
-  
-  const course = await Course.findById(req.params.id);
-  if (!course) {
-    return res.status(404).json({ message: "course not found" });
+  try {
+    const course = await Course.findById(req.params.id);
+    if (!course) {
+      return res.status(404).json({ message: "course not found" });
+    }
+    res.json(course);
+  } catch (err) {
+    return res.status(400).json({ message: "Invalid Obj Id" });
   }
-  res.json(course);
 };
 
 const addCourse = async (req, res) => {
@@ -27,37 +29,35 @@ const addCourse = async (req, res) => {
   res.status(201).json(newCourse);
 };
 
-const updateCourse = (req,res)=>{
+const updateCourse = (req, res) => {
   const id = +req.params.id;
-  let course = courses.find(cor=>cor.id === id);
+  let course = courses.find((cor) => cor.id === id);
   if (!course) {
-    return res.status(404).json({message:"course not found"});
+    return res.status(404).json({ message: "course not found" });
   }
-  courses[id-1] = {...course,...req.body};
-  fs.writeFile('./data/db.json',JSON.stringify(courses),'utf-8',(err)=>{
+  courses[id - 1] = { ...course, ...req.body };
+  fs.writeFile("./data/db.json", JSON.stringify(courses), "utf-8", (err) => {
     if (err) {
       throw err;
     }
   });
-  res.status(200).json(courses[id-1]);
- 
+  res.status(200).json(courses[id - 1]);
 };
 
-const deleteCourse = (req,res)=>{
+const deleteCourse = (req, res) => {
   const id = +req.params.id;
-  let course = courses.find(course => course.id === id);
+  let course = courses.find((course) => course.id === id);
   if (!course) {
-    return res.status(404).json({message:"course not found"});
+    return res.status(404).json({ message: "course not found" });
   }
   //courses.splice(id-1,1);
-  courses = courses.filter(course => course.id !== id);
-  fs.writeFile('./data/db.json',JSON.stringify(courses),'utf-8',(err)=>{
+  courses = courses.filter((course) => course.id !== id);
+  fs.writeFile("./data/db.json", JSON.stringify(courses), "utf-8", (err) => {
     if (err) {
       throw err;
     }
   });
   res.status(200).json(courses);
- 
 };
 
 module.exports = {
@@ -65,5 +65,5 @@ module.exports = {
   getSingleCourse,
   addCourse,
   updateCourse,
-  deleteCourse
+  deleteCourse,
 };
