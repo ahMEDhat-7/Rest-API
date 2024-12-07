41e5334bb01148c347103d2bbbe58eea83b19804
ahmed medhat <am510836@gmail.com>
2024-12-07 05:28:31 +0200

    register and login

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,16 +1,21 @@
 const express = require("express");
-const {validationUsersSchema} = require('../middlewares/validationSchema');
-const usersController = require('../controllers/users.controller');
+const { validationUsersSchema } = require("../middlewares/validationSchema");
+const usersController = require("../controllers/users.controller");
 const router = express.Router();
 
-router.route("/") //    /api/users
-            .get(usersController.getAllUsers)
-            .post(validationUsersSchema(),usersController.addUser);
+router
+  .route("/") //    /api/users
+  .get(usersController.getAllUsers);
+  // .post(validationUsersSchema(), usersController.addUser);
 
-router.route("/:id")
-            .get(usersController.getSingleUser)
-            .patch(validationUsersSchema(),usersController.updateUser)
-            .delete(usersController.deleteUser);
+router
+  .route("/:id")
+  .get(usersController.getSingleUser)
+  .patch(validationUsersSchema(), usersController.updateUser)
+  .delete(usersController.deleteUser);
 
+router.route("/register").post(validationUsersSchema(),usersController.addUser);
+
+router.route("/login").post(usersController.getSingleUser);
 
 module.exports = router;
